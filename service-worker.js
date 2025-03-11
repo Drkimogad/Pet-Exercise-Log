3cba89b65bab5765d63cf20f638ba0148c7d6142
Kmgad <189161009+Drkimogad@users.noreply.github.com>
2025-03-11 16:07:01 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,7 @@ const urlsToCache = [
     'https://drkimogad.github.io/Pet-Exercise-Log/',                // Main page URL
     'https://drkimogad.github.io/Pet-Exercise-Log/index.html',      // Ensure main HTML page is cached
     'https://drkimogad.github.io/Pet-Exercise-Log/styles.css',
-    'https://drkimogad.github.io/Pet-Exercise-Log/script.js',
+    'https://drkimogad.github.io/Pet-Exercise-Log/app.js',
     'https://drkimogad.github.io/Pet-Exercise-Log/manifest.json',
     'https://drkimogad.github.io/Pet-Exercise-Log/icons/icon-192x192.png',
     'https://drkimogad.github.io/Pet-Exercise-Log/favicon.ico',
