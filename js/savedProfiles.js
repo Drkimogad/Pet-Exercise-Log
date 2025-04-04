51da1cb602290f4a47ebc09edd99ff40e31f5a27
Kmgad <189161009+Drkimogad@users.noreply.github.com>
2025-04-04 21:53:14 +0200

    Update savedProfiles.js

--- js/savedProfiles.js
+++ js/savedProfiles.js
@@ -1,6 +1,6 @@
-import dataService from '/Pet-Exercise-Log/js/dataService.js';
-import * as PetFormSection from '/Pet-Exercise-Log/js/petEntry.js'; // Import PetFormSection
-import * as ReportGenerator from '/Pet-Exercise-Log/js/monthlyReport.js'; // Import ReportGenerator
+import dataService from 'https://drkimogad.github.io/Pet-Exercise-Log/js/dataService.js';
+import * as PetFormSection from 'https://drkimogad.github.io/Pet-Exercise-Log/js/petEntry.js'; // Import PetFormSection
+import * as ReportGenerator from 'https://drkimogad.github.io/Pet-Exercise-Log/js/monthlyReport.js'; // Import ReportGenerator
 
 const SavedProfilesSection = (function() {
   const renderSavedProfiles = () => {
